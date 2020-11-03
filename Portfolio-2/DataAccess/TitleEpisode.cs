b66adc9dfed029d2584ae971439a33696beecb84
emilieunna <ebul@ruc.dk>
2020-11-03 14:22:43 +0100

    titleepisode

--- Portfolio-2/DataAccess/TitleEpisode.cs
+++ Portfolio-2/DataAccess/TitleEpisode.cs
@@ -2,6 +2,12 @@ namespace DataAccess
 {
     public class TitleEpisode
     {
-        
+        public string Tconst { get; set; }
+
+        public string ParentTconst { get; set; }
+
+        public int SeasonNumber { get; set; }
+
+        public int EpisodeNumber { get; set; }
     }
 }
