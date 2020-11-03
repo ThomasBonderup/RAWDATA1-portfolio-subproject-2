abc3b2b58b1bec60c9b4e8380507f73b4f0c2b1e
emilieunna <ebul@ruc.dk>
2020-11-03 14:21:29 +0100

    titleepisode

--- Portfolio-2/DataAccess/Name.cs
+++ Portfolio-2/DataAccess/Name.cs
@@ -2,6 +2,12 @@ namespace DataAccess
 {
     public class Name
     {
+        public string Nconst { get; set; }
         
+        public string PrimaryName { get; set; }
+
+        public int BirthYear { get; set; }
+
+        public int DeathYear { get; set; }
     }
 }
