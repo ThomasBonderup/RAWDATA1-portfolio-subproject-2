5b9d8dcb666a5884fa6104b36f3afb99ca75e57f
emilieunna <ebul@ruc.dk>
2020-11-03 14:00:57 +0100

    title class

--- Portfolio-2/DataAccess/Title.cs
+++ Portfolio-2/DataAccess/Title.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataAccess
 {
     public class Title
     {
+        public string tconst { get; set; }
+
+        public string titletype { get; set; }
+
+        public string primaryTitle { get; set; }
+
+        public string originalTitle { get; set; }
         
+        public bool isAdult { get; set; } 
+        
+        public int startYear { get; set; }
+
+        public int endYear { get; set; }
+
+        public int runTimeMinutes { get; set; }
+
+        public string genre { get; set;}
     }
 }
