25597501e8b9688a77ad1a18ebbafd3972e17bcd
mullenborn <nilsmullenborn@hotmail.com>
2020-11-05 14:04:04 +0100

    added webservices test file

--- Portfolio-2/UnitTests/UnitTest1.cs
+++ Portfolio-2/UnitTests/UnitTest1.cs
@@ -21,30 +21,7 @@ namespace UnitTests
             _testOutputHelper = testOutputHelper;
         }
 
-        // USE THESE IN WEB SERVICE TEST
-        /*
-        // TITLES
-        private const string TitlesApi = "http://localhost:5001/api/titles";
-        private const string TitlePrincipalsApi = "http://localhost:5001/api/titles/titleprincipals";
-        private const string TitleGenresApi = "http://localhost:5001/api/titles/titlegenres";
-        private const string LocalTitlesApi = "http://localhost:5001/api/titles/localtitle";
-        private const string TitleRatingsApi = "http://localhost:5001/api/titles/titleratings";
-        // USERS 
-        private const string UsersApi = "http://localhost:5001/api/users";
-        private const string TitleNotesApi = "http://localhost:5001/api/users/titlenotes";
-        private const string TitleBookmarksApi = "http://localhost:5001/api/users/titlebookmarks";
-        private const string NameNotesApi = "http://localhost:5001/api/users/namenotes";
-        private const string NameBookmarks = "http://localhost:5001/api/users/namebookmarks";
-        private const string SearchHistoryApi = "http://localhost:5001/api/users/searchhistory";
-        private const string RatingApi = "http://localhost:5001/api/users/rating";
-        private const string RatingHistoryApi = "http://localhost:5001/api/users/rating/ratinghistory";
-        // NAMES
-        private const string NamesApi = "http://localhost:5001/api/names";
-        private const string KnownForTitles = "http://localhost:5001/api/names/knownfortitles";
-        private const string NameRatingApi = "http://localhost:5001/api/names/namerating";
-        private const string PrimaryProfessionApi = "http://localhost:5001/api/names/primaryprofession";
-       // private const string TitlePrincipalsApi = "http://localhost:5001/api/names/titleprincipals";
-        */
+        
 
         // /api/titles
 
---
+++ Portfolio-2/UnitTests/WebServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using Xunit;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using DataAccess;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class WebServiceTests
+    {
+        // api paths 
+        // TITLES
+        private const string TitlesApi = "http://localhost:5001/api/titles";
+        private const string TitlePrincipalsApi = "http://localhost:5001/api/titles/titleprincipals";
+        private const string TitleRatingsApi = "http://localhost:5001/api/titles/titleratings";
+        // USERS 
+        private const string UsersApi = "http://localhost:5001/api/users";
+        private const string TitleNotesApi = "http://localhost:5001/api/users/titlenotes";
+        private const string TitleBookmarksApi = "http://localhost:5001/api/users/titlebookmarks";
+        private const string NameNotesApi = "http://localhost:5001/api/users/namenotes";
+        private const string NameBookmarks = "http://localhost:5001/api/users/namebookmarks";
+        private const string SearchHistoryApi = "http://localhost:5001/api/users/searchhistory";
+        private const string RatingApi = "http://localhost:5001/api/users/rating";
+        private const string RatingHistoryApi = "http://localhost:5001/api/users/rating/ratinghistory";
+        // NAMES
+        private const string NamesApi = "http://localhost:5001/api/names";
+        private const string KnownForTitles = "http://localhost:5001/api/names/knownfortitles";
+        private const string NameRatingApi = "http://localhost:5001/api/names/namerating";
+        private const string PrimaryProfessionApi = "http://localhost:5001/api/names/primaryprofession";
+       // private const string TitlePrincipalsApi = "http://localhost:5001/api/names/titleprincipals";
+        
+        
+    }
+}
