df713a6af6be3f00f2b21aaa10d92c3f5df1214c
emilieunna <ebul@ruc.dk>
2020-11-05 11:14:37 +0100

    first try NameListDto

--- Portfolio-2/DataAccess/NameListDto.cs
+++ Portfolio-2/DataAccess/NameListDto.cs
@@ -2,6 +2,15 @@ namespace DataAccess
 {
     public class NameListDto
     {
+        public string Url { get; set; }
         
+        public string Nconst { get; set; }
+
+        public string PrimaryName { get; set; }
+        
+        public int BirthYear { get; set; }
+        
+        public int DeathYear { get; set; }
+
     }
 }
