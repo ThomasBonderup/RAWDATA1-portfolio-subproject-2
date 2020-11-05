822cbbcc5098ad77df01b3ed4ac4c9d97924790e
emilieunna <ebul@ruc.dk>
2020-11-05 09:41:16 +0100

    User class

--- Portfolio-2/DataAccess/User.cs
+++ Portfolio-2/DataAccess/User.cs
@@ -2,6 +2,21 @@ namespace DataAccess
 {
     public class User
     {
-        
+        public string Uconst { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Uconst = {Uconst}, FirstName = {FirstName}, LastName = {LastName}, Email = {Email}," +
+                   $"Password = {Password}, UserName ={UserName}";
+        }
     }
 }
