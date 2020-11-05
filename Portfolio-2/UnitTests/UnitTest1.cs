fabe3b0a506098003c3c6662003e7b076d030005
Alex Tao <alexwoko@gmail.com>
2020-11-05 09:26:34 +0100

    Localhost

--- Portfolio-2/UnitTests/UnitTest1.cs
+++ Portfolio-2/UnitTests/UnitTest1.cs
@@ -9,24 +9,24 @@ namespace UnitTests
         // TITLES
         private const string TitlesApi = "http://localhost:5001/api/titles";
         private const string TitlesPrincipalsApi = "http://localhost:5001/api/titles/titleprincipals";
-        private const string TitleGenresApi = "/api/titles/titlegenres";
-        private const string LocalTitlesApi = "/api/titles/localtitle";
-        private const string TitleRatingsApi = "/api/titles/titleratings";
+        private const string TitleGenresApi = "http://localhost:5001/api/titles/titlegenres";
+        private const string LocalTitlesApi = "http://localhost:5001/api/titles/localtitle";
+        private const string TitleRatingsApi = "http://localhost:5001/api/titles/titleratings";
         // USERS 
-        private const string UsersApi = "/api/users";
-        private const string TitleNotesApi = "/api/users/titlenotes";
-        private const string TitleBookmarksApi = "/api/users/titlebookmarks";
-        private const string NameNotesApi = "/api/users/namenotes";
-        private const string NameBookmarks = "/api/users/namebookmarks";
-        private const string SearchHistoryApi = "/api/users/searchhistory";
-        private const string RatingApi = "/api/users/rating";
-        private const string RatingHistoryApi = "/api/users/rating/ratinghistory";
+        private const string UsersApi = "http://localhost:5001/api/users";
+        private const string TitleNotesApi = "http://localhost:5001/api/users/titlenotes";
+        private const string TitleBookmarksApi = "http://localhost:5001/api/users/titlebookmarks";
+        private const string NameNotesApi = "http://localhost:5001/api/users/namenotes";
+        private const string NameBookmarks = "http://localhost:5001/api/users/namebookmarks";
+        private const string SearchHistoryApi = "http://localhost:5001/api/users/searchhistory";
+        private const string RatingApi = "http://localhost:5001/api/users/rating";
+        private const string RatingHistoryApi = "http://localhost:5001/api/users/rating/ratinghistory";
         // NAMES
-        private const string NamesApi = "/api/names";
-        private const string KnownForTitles = "/api/names/knownfortitles";
-        private const string NameRatingApi = "/api/names/namerating";
-        private const string PrimaryProfessionApi = "/api/name/primaryprofession";
-        private const string TitlePrincipalsApi = "/api/names/titleprincipals";
+        private const string NamesApi = "http://localhost:5001/api/names";
+        private const string KnownForTitles = "http://localhost:5001/api/names/knownfortitles";
+        private const string NameRatingApi = "http://localhost:5001/api/names/namerating";
+        private const string PrimaryProfessionApi = "http://localhost:5001/api/name/primaryprofession";
+        private const string TitlePrincipalsApi = "http://localhost:5001/api/names/titleprincipals";
         
         
         [Fact]
