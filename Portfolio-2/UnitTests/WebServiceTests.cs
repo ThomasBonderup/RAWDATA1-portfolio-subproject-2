4d578b576ffd573046d87526e03712cc3b11935f
mullenborn <nilsmullenborn@hotmail.com>
2020-11-10 14:07:32 +0100

    First WebServiceTest complete

--- Portfolio-2/UnitTests/WebServiceTests.cs
+++ Portfolio-2/UnitTests/WebServiceTests.cs
@@ -10,31 +10,148 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit.Abstractions;
 
+
 namespace UnitTests
 {
     public class WebServiceTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public WebServiceTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         // api paths 
-        // TITLES
         private const string TitlesApi = "http://localhost:5001/api/titles";
-        private const string TitlePrincipalsApi = "http://localhost:5001/api/titles/titleprincipals";
-        private const string TitleRatingsApi = "http://localhost:5001/api/titles/titleratings";
-        // USERS 
-        private const string UsersApi = "http://localhost:5001/api/users";
-        private const string TitleNotesApi = "http://localhost:5001/api/users/titlenotes";
-        private const string TitleBookmarksApi = "http://localhost:5001/api/users/titlebookmarks";
-        private const string NameNotesApi = "http://localhost:5001/api/users/namenotes";
-        private const string NameBookmarks = "http://localhost:5001/api/users/namebookmarks";
-        private const string SearchHistoryApi = "http://localhost:5001/api/users/searchhistory";
-        private const string RatingApi = "http://localhost:5001/api/users/rating";
-        private const string RatingHistoryApi = "http://localhost:5001/api/users/rating/ratinghistory";
-        // NAMES
+        private const string UsersApi = "http://localhost:5001/api/users"; 
         private const string NamesApi = "http://localhost:5001/api/names";
-        private const string KnownForTitles = "http://localhost:5001/api/names/knownfortitles";
-        private const string NameRatingApi = "http://localhost:5001/api/names/namerating";
-        private const string PrimaryProfessionApi = "http://localhost:5001/api/names/primaryprofession";
-       // private const string TitlePrincipalsApi = "http://localhost:5001/api/names/titleprincipals";
-        
-        
+
+
+        // api/titles
+       [Fact]
+       public void ApiTitles_GetWithNoArguments_OkAndAllTitles()
+       {
+           var (data, statusCode) = GetObject(TitlesApi);
+           var list = data["items"];
+           //_testOutputHelper.WriteLine(list.ToString());
+           
+           Assert.Equal(HttpStatusCode.OK, statusCode);
+           Assert.Equal(10, list.Count());
+           Assert.Equal("tt10850402", list.First()["tconst"]);
+           Assert.Equal("tt2583620 ", list.Last()["tconst"]);
+       }
+
+       /*[Fact]
+       public void ApiTitles_GetWithValidTitleId_OkAndTitle()
+       {
+           var (title, statusCode) = GetObject($"{TitlesApi}/tt9999998");
+           
+           Assert.Equal(HttpStatusCode.OK, statusCode);
+           Assert.Equal("Test 1 primarytitle", title["primaryname"]);
+       }
+       
+       [Fact]
+       public void ApiTitles_GetWithInvalidTitleId_NotFound()
+       {
+           var (_, statusCode) = GetObject($"{TitlesApi}/tt00000000");
+           
+           Assert.Equal(HttpStatusCode.NotFound, statusCode);
+       }
+
+
+       // api/users
+       [Fact]
+       public void ApiUsers_GetWithNoArguments_OkAndAllUsers()
+       {
+           var (data, statusCode) = GetArray(UsersApi);
+           
+           Assert.Equal(HttpStatusCode.OK, statusCode);
+           Assert.Equal(6, data.Count);
+           Assert.Equal("ui000001", data.First()["uconst"]);
+           Assert.Equal("ui000006", data.Last()["uconst"]);
+       }
+       
+       [Fact]
+       public void ApiUsers_GetWithValidUserId_OkAndUser()
+       {
+           var (user, statusCode) = GetObject($"{UsersApi}/ui000002");
+           
+           Assert.Equal(HttpStatusCode.OK, statusCode);
+           Assert.Equal("Nils", user["firstmame"]);
+       }
+       
+       [Fact]
+       public void ApiUsers_GetWithInvalidUserId_NotFound()
+       {
+           var (_, statusCode) = GetObject($"{UsersApi}/ui000000");
+           
+           Assert.Equal(HttpStatusCode.NotFound, statusCode);
+       }
+       
+       
+       [Fact]
+       // api/names
+       public void ApiNames_GetWithNoArguments_OkAndAllNames()
+       {
+           var (data, statusCode) = GetArray(NamesApi);
+           
+           Assert.Equal(HttpStatusCode.OK, statusCode);
+           Assert.Equal(234485, data.Count);
+           Assert.Equal("nm0000001", data.First()["nconst"]);
+           Assert.Equal("nm99999999", data.Last()["nconst"]);
+       }*/
+       
+       
+       
+       
+       // Helper methods for tests
+
+       (JArray, HttpStatusCode) GetArray(string url)
+       {
+           var client = new HttpClient();
+           var response = client.GetAsync(url).Result;
+           var data = response.Content.ReadAsStringAsync().Result;
+           return ((JArray)JsonConvert.DeserializeObject(data), response.StatusCode);
+       }
+
+       (JObject, HttpStatusCode) GetObject(string url)
+       {
+           var client = new HttpClient();
+           var response = client.GetAsync(url).Result;
+           var data = response.Content.ReadAsStringAsync().Result;
+           return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
+       }
+
+       (JObject, HttpStatusCode) PostData(string url, object content)
+       {
+           var client = new HttpClient();
+           var requestContent = new StringContent(
+               JsonConvert.SerializeObject(content),
+               Encoding.UTF8,
+               "application/json");
+           var response = client.PostAsync(url, requestContent).Result;
+           var data = response.Content.ReadAsStringAsync().Result;
+           return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
+       }
+
+       HttpStatusCode PutData(string url, object content)
+       {
+           var client = new HttpClient();
+           var response = client.PutAsync(
+               url,
+               new StringContent(
+                   JsonConvert.SerializeObject(content),
+                   Encoding.UTF8,
+                   "application/json")).Result;
+           return response.StatusCode;
+       }
+
+       HttpStatusCode DeleteData(string url)
+       {
+           var client = new HttpClient();
+           var response = client.DeleteAsync(url).Result;
+           return response.StatusCode;
+       }
     }
 }
