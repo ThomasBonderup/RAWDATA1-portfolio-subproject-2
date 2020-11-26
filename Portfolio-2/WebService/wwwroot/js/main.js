c145e71adb301acfcfa910495c5b99394f18038d
ThomasBonderup <tb@tbcoding.dk>
2020-11-26 13:33:24 +0100

    add dataService.js

--- Portfolio-2/WebService/wwwroot/js/main.js
+++ Portfolio-2/WebService/wwwroot/js/main.js
@@ -1,7 +1,8 @@
 require.config({
     baseUrl: "js",
     paths : {
-        knockout: "lib/knockout/knockout-latest"
+        knockout: "lib/knockout/knockout-latest",
+        dataservice: "services/dataService"
     }
 });
 
