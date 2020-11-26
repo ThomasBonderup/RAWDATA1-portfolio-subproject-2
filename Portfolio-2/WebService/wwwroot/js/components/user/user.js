73e227f47f87ab5ede12dc5aab4ee16dda8c4d43
Alex Tao <alexwoko@gmail.com>
2020-11-26 15:27:21 +0100

    User component

---
+++ Portfolio-2/WebService/wwwroot/js/components/user/user.js
@@ -0,0 +1,23 @@
+define(['knockout'], ['dataservice'], (ko, ds, user) =>{
+   
+    let userImg = ko.observable();
+    let firstName = ko.observable("Django");
+    let lastName = ko.observable("Bang-Bang");
+    
+    let ratingHistory = ko.observableArray();
+    let bookmarks = ko.observableArray();
+    
+    
+    
+    
+    
+    return {
+        
+        userImg,
+        firstName,
+        lastName,
+        ratingHistory,
+        bookmarks
+    }
+    
+});
