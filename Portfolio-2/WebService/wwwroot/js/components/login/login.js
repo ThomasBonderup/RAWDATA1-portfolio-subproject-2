2df4d434d9b2136870ee16e82d0f9a2c2b569dc2
emilieunna <ebul@ruc.dk>
2020-11-26 14:11:03 +0100

    login component

--- Portfolio-2/WebService/wwwroot/js/components/login/login.js
+++ Portfolio-2/WebService/wwwroot/js/components/login/login.js
@@ -1,4 +1,15 @@
-define(['knockout'], function(login) => {
+define(['knockout'], function(login){
+    var newUser = login.observable();
     
+    function signIn(signIn){
+        this.signIn = login.observable(signIn);
+    }
+    
+    let loginBtn = () => console.log("Login button clicked");
+    
+    return{
+        newUser,
+        loginBtn
+    }
     
 });
