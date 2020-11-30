12eb3be838c261e66309d370982bb07be3a926aa
emilieunna <ebul@ruc.dk>
2020-11-30 15:27:47 +0100

    drop down login

--- Portfolio-2/WebService/wwwroot/js/components/login/login.js
+++ Portfolio-2/WebService/wwwroot/js/components/login/login.js
@@ -1,8 +1,22 @@
 define(['knockout'], function(login){
     var newUser = login.observable();
     
+    //supposed to  - when the user clicks on the button, toggle between hiding and showing the dropdown content(?)
     function loginFunction(){
         document.getElementById("dropdownLogin").classList.toggle("show");
+    }
+    
+    window.onclick = function(event){
+        if(!event.target.matches('.dropBtn')){
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i=0; i<dropdowns.length; i++){
+                var openDropdown = dropdowns[i];
+                if(openDropdown.classList.contains('show')){
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
     }
     
    /* let loginBtn = () => console.log("Login button clicked");
