069739bba58998e881b79e028c5025637691cccb
ThomasBonderup <tb@tbcoding.dk>
2020-12-01 12:42:24 +0100

    change knockout js library minified to debug version

--- Portfolio-2/WebService/wwwroot/js/main.js
+++ Portfolio-2/WebService/wwwroot/js/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: "js",
     paths : {
-        knockout: "lib/knockout/knockout-latest",
+        knockout: "lib/knockout/knockout-latest.debug",
         text: "lib/require-text/text.min",
         dataservice: "services/dataService"
     }
