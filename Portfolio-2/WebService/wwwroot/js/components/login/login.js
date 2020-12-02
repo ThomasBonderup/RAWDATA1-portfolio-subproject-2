0802e601c55e0f94894984780b802fab2b90c30a
emilieunna <ebul@ruc.dk>
2020-12-02 10:25:07 +0100

    login component

--- Portfolio-2/WebService/wwwroot/js/components/login/login.js
+++ Portfolio-2/WebService/wwwroot/js/components/login/login.js
@@ -1,25 +1,32 @@
-define(['knockout'], function(login){
-    var newUser = [
-        {firstName: login.observable(), lastName:login.observable(), email:login.observable(),username:login.observable(), password:login.observable()}
-    ];
-    let selectedUser = login.observable();
-    
-    let createNewUser = () =>{
-        newUser.push(selectedUser());
+define(['knockout'], (login)=> {
+    return function (params) {
+        var newUser =
+            {
+                firstName: login.observable(),
+                lastName: login.observable(),
+                email: login.observable(),
+                username: login.observable(),
+                password: login.observable()
+            }
+
+        let selectedUser = login.observable();
+
+        let createNewUser = () => {
+            newUser.push(selectedUser());
+        }
+        let registerBtn = () =>
+            console.log("registerbtn clicked");
+
+        //supposed to  - when the user clicks on the button, toggle between hiding and showing the dropdown content(?)
+        //      let registerBtn = () => console.log("Login button clicked");
+
+        let signInBtn = () => console.log("Sign in button clicked");
+
+        return {
+            newUser,
+            selectedUser,
+            createNewUser,
+            registerBtn
+        }
     }
-    let registerBtn = function(){
-        console.log("registerbtn clicked");
-    };
-    //supposed to  - when the user clicks on the button, toggle between hiding and showing the dropdown content(?)
-  //  let registerBtn = () => console.log("Login button clicked");
-    
-    let signInBtn = () => console.log("Sign in button clicked");
-    
-    return{
-        newUser,
-        selectedUser,
-      createNewUser,
-       registerBtn
-    }
-    
 });
