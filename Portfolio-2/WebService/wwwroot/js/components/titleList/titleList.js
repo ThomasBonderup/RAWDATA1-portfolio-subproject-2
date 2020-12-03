0866c2b915bd19aae3f4b2ce959bdb00f16361a1
ThomasBonderup <tb@tbcoding.dk>
2020-12-03 17:00:41 +0100

    add click function to prev and next buttons

--- Portfolio-2/WebService/wwwroot/js/components/titleList/titleList.js
+++ Portfolio-2/WebService/wwwroot/js/components/titleList/titleList.js
@@ -2,10 +2,27 @@ define(['knockout', 'dataservice'], (ko, ds) =>  {
    return function (params) {
        let titles = ko.observableArray([]);
        let searchString = params.searchInput;
-       ds.getTitles(ko.unwrap(searchString), function (data) { titles(data)});
+       let prev = ko.observable();
+       let next = ko.observable();
+       
+       let showPrev = title => {
+           console.log(prev());
+       }
+
+       let showNext = title => {
+           console.log(next());
+       }
+       
+       ds.getTitles(ko.unwrap(searchString), function (data) {
+           prev(data.prev);
+           next(data.next);
+           titles(data);
+       });
        //debugger;
        //console.log(ko.toJSON(titles));
        return {
+           showPrev,
+           showNext,
            titles
        }
    } 
