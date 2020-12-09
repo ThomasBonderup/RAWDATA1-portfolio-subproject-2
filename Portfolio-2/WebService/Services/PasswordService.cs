aa0f87142f12a20b132fa30f4ae53fcf92fa4192
emilieunna <ebul@ruc.dk>
2020-12-09 16:30:56 +0100

    authentication

--- Portfolio-2/WebService/Services/PasswordService.cs
+++ Portfolio-2/WebService/Services/PasswordService.cs
@@ -1,7 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Npgsql.TypeHandlers;
+
 namespace WebService.Services
 {
     public class PasswordService
     {
-        
+        private static RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+
+        public static string HashPassword(string password, string salt, int size)
+        {
+            var hash = KeyDerivation.Pbkdf2(
+                password,
+                Encoding.UTF8.GetBytes(salt),
+                KeyDerivationPrf.HMACSHA256,
+                10000,
+                size);
+            
+            return Convert.ToBase64String(hash);
+        }
+
+        public static string GenerateSalt(int size)
+        {
+            var buffer = new byte[size];
+            _rng.GetBytes(buffer);
+            return Convert.ToBase64String(buffer);
+        }
     }
 }
