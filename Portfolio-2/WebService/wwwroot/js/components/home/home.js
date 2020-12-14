142a9b0d0174248845f0bba5d610046dc8e0c076
skynet <tb@tbcoding.dk>
2020-12-14 11:00:58 +0100

    remove login from home page

--- Portfolio-2/WebService/wwwroot/js/components/home/home.js
+++ Portfolio-2/WebService/wwwroot/js/components/home/home.js
@@ -1,19 +1,7 @@
 ﻿define(["knockout","dataservice"], (ko, ds)=> {
     return function (params) {
 
-        let username = ko.observable();
-        let password = ko.observable();
-
-        let loginUser = (function(data) {
-            console.log(username());
-            console.log(password());
-            ds.userLogin(username(), password());
-        });
-
         return {
-            username,
-            password,
-            loginUser
         }
     }
 });
